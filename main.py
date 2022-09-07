8d2d8b66c3fb7aabe460b3a83417d701d62deaf2
Lya Carolina <38046847+lyacarolina@users.noreply.github.com>
2022-09-07 14:22:27 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from quadrado import Quadrado
-
-def main():
-    calculo = Quadrado(3)
-    print("A área é de {} m².".format(calculo.area()))
-    print("O perímetro é de {} m.".format(calculo.perimetro()))
-      
-main()
-        
